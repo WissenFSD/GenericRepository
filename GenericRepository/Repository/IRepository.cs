a852fb814d562d34e6918cad1a468f4d9230ce1d
wissenfsd@gmail.com <wissenfsd@gmail.com>
2024-04-03 21:38:27 +0300

    Add project files.

---
+++ GenericRepository/Repository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace GenericRepository.Repository
+{
+	public interface IRepository<TEntity>
+	{
+		// Generic repository'de aşağıdaki metotlar olmalıdır.
+		IQueryable<TEntity> GetAll();
+		TEntity GetById(int id);
+		void Create(TEntity entity);
+
+		void Update(int id, TEntity entity);
+
+		void Delete(int id);
+	}
+}
---
+++ GenericRepository/Repository/ProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using GenericRepository.DMO;
+
+namespace GenericRepository.Repository
+{
+    public class ProductRepository : GenericRepository<Product>
+    {
+        public ProductRepository(AdventureWorks2019Context context) : base(context)
+        {
+
+        }
+    }
+}
